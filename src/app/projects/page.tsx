8ee57505694b3e3e32679a3318d64dbecafcbcc6
Haseeb Ahmed Raza Khan <web.dev.haseeb@gmail.com>
2025-06-02 09:24:08 +0500

    build fix

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,7 +6,7 @@ export default async function Works() {
 
     return (
         <>
-            <h1 className="max-w-[85vw] mx-auto w-full md:pt-[8vw] pt-[10vh] md:pb-[8vw] pb-[15vw] poppins text-[6.5vw] lg:text-[3.5vw] leading-[120%] tracking-tight">
+            <h1 className="max-w-[85vw] mx-auto w-full md:pt-[8vw] pt-[10vh] md:pb-[8vw] pb-[15vw] text-[8vw] lg:text-[3.5vw] leading-[130%] font-medium tracking-tight">
                 These are the websites and apps I’ve built. Unlike most agency work, they’re built with care, speed, and a deep focus on what actually works.
             </h1>
             <div className="w-full lg:mb-[10vw] mb-[20vw]">
